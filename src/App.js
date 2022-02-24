9f57f37eadbb52cc295c85d7cb383a7544d621a9
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-02-24 16:01:57 -0300

    Subindo os arquivos de código do uso do Redux em aplicações React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    const { newValue } = this.props;
+    return (
+      <div className="App" style={{ paddingTop: '10px' }}>
+        <input type='text' />
+        <button>
+          Click me!
+        </button>
+        <h1>{newValue}</h1>
+      </div>
+    );
+  }
+}
+const mapStateToProps = store => ({
+  newValue: store.clickState.newValue
+});
+
+export default connect(mapStateToProps)(App);
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { Store } from './store';
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
+reportWebVitals();
